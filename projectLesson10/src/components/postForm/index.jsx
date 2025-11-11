44dc329c0f2720bcda703d4ee612abbb303b7f34
Anna <anshalneva@gmail.com>
2025-11-11 18:48:28 +0100

    Added logic of component PostForm

--- projectLesson10/src/components/postForm/index.jsx
+++ projectLesson10/src/components/postForm/index.jsx
@@ -1,16 +1,22 @@
 import { useForm } from "react-hook-form";
-import { PostProvider, usePostContext } from "../../context/PostsContext";
+import { usePostContext } from "../../context/PostsContext";
 
-const {state,actions} = usePostContext
+
 export default function PostForm(){
-  const {register,handleSubmit,formState:{errors}} = useForm();
-function onSubmit(){}
+  const {actions} = usePostContext()
+  const {addPost} = actions
+  const {register,handleSubmit,reset, formState:{errors}} = useForm();
+function onSubmit(data){
+const newPost = {title:data.title,text: data.text,avatar:"https://avatars.githubusercontent.com/u/96827151"}
+addPost(newPost)
+reset()
+}
   return(
     <div>
           <form onSubmit={handleSubmit(onSubmit)} >
-<input type="text" {...register("title",{required: "You shoukd add the title"})} />
+<input type="text" {...register("title",{required: "You should add the title"})} />
 {errors.title && <p>{errors.title.message}</p>}
-<input type="text" {...register("text",{required: "You shoukd add the title"})} />
+<input type="text" {...register("text",{required: "You should add the text"})} />
 {errors.text && <p>{errors.text.message}</p>}
 <button>Send post</button>
     </form>

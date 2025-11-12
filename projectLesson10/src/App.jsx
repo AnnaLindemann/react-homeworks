4c9d65b14c88220db0e50711260a5d29a4789853
Anna <anshalneva@gmail.com>
2025-11-12 12:58:09 +0100

    Added Post component logic and updated imports in App

--- projectLesson10/src/App.jsx
+++ projectLesson10/src/App.jsx
@@ -1,13 +1,24 @@
 import Header from './components/header'
+import PostForm from './components/postForm'
+import PostList from './components/postList'
+import { PostProvider } from './context/PostsContext'
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
+    <PostProvider>
 <Header/>
-    </>
+<div>
+  <div>
+<h2>List of Posts</h2>
+<PostList/></div>
+<div>
+  <h2>Write the post</h2>
+  <PostForm/>
+</div>
+   </div> </PostProvider>
   )
 }
 
--- projectLesson10/src/components/post/index.jsx
+++ projectLesson10/src/components/post/index.jsx
@@ -1,1 +1,20 @@
-export default function Post(){}
+export default function Post({onDelete, id, title,text,avatar}){
+
+  return(
+    <div>
+   <div><img src={avatar} alt="Avatar" />
+   <h4>User Logo</h4>
+   </div>
+   <div>
+<h3>{title}</h3>
+<p>{text}</p>
+   </div>
+   <div>
+    <h3>{id}</h3>
+    <button onClick={onDelete}>Delete</button>
+   </div>
+    </div>
+  )
+
+
+}

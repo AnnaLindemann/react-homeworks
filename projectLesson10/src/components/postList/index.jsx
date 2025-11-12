693806611accbf8338085a0c262abe50c5c0cb84
Anna <anshalneva@gmail.com>
2025-11-12 12:39:19 +0100

    Added logic for the component PostList

---
+++ projectLesson10/src/components/postList/index.jsx
@@ -0,0 +1,36 @@
+import { usePostContext } from "../../context/PostsContext";
+import Post from "../post";
+
+export default function PostList(){
+  const{state,actions} = usePostContext()
+  const {deletePost,nextPage, prevPage} = actions
+  const {isLoading,error,posts,currentPage,pageSize} = state
+
+
+if(isLoading === true){
+  return <p>Loading...</p>
+} else if(!isLoading && error){
+  return <p>Oops!Something went wrong!</p>
+}
+else if(!isLoading && posts.length === 0) {
+  return <p>There is not post yet</p>
+}
+
+else {
+  const start = (currentPage - 1) * pageSize;
+  const end = start + pageSize
+  const visablePosts = posts.slice(start,end)
+  const totalPages = Math.ceil(posts.length / pageSize);
+  return(
+    <div>
+      {visablePosts.map((visablePost) => 
+         <div key={visablePost.id}><Post onDelete={()=> deletePost(visablePost.id)} id={visablePost.id} title = {visablePost.title} text={visablePost.text} avatar={visablePost.avatar}/></div>)
+      }
+      <button onClick={prevPage} disabled={currentPage === 1}>Previosly</button>
+      <button onClick={nextPage} disabled={currentPage === totalPages || totalPages === 0}>Next</button>
+    </div>
+    )
+}
+
+
+}

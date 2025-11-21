cef1788b07401a9aac05d471f9cb2dc93244b043
Anna <anshalneva@gmail.com>
2025-11-21 18:23:20 +0100

    Added component ValueDisplay

--- hw12/src/App.jsx
+++ hw12/src/App.jsx
@@ -1,12 +1,25 @@
-
+import { useState } from 'react';
 import './App.css'
-
+import ValueDisplay from './components/valueDisplay';
 
 function App() {
+const [value,setValue] = useState("");
 
 
+function handleOnChange(e){
+  e.preventDefault();
+const updatedInput = e.target.value;
+setValue(updatedInput);
+
+}
   return (
-   
+    <div>
+      <h2>Current and previous Value</h2>
+    <form onChange={handleOnChange}>
+      <input type="text" placeholder='Insert your text.' />
+     <ValueDisplay value={value}/>
+    </form>
+    </div>
   )
 }
 
--- hw12/src/components/valueDisplay/index.jsx
+++ hw12/src/components/valueDisplay/index.jsx
@@ -1,1 +1,21 @@
-export default function ValueDisplay(){}
+import { useEffect,useRef } from "react";
+import styles from "./styles.module.css"
+export default function ValueDisplay({value}){
+const prevValueRef = useRef("");
+
+useEffect(()=> {
+
+prevValueRef.current = value;
+
+
+},[value])
+
+
+return(
+  <div className={styles.valueCont}>
+    <span>Current value:{value}</span>
+      <span  >Previous value:{prevValueRef.current}</span>
+  </div>
+)
+
+ }

6072eda9ca7f02d20428d71c655c89ac9feaabbb
Anna <anshalneva@gmail.com>
2025-12-04 08:26:56 +0100

    Feat:save user auth data to localStorage

--- practiceLogIn/src/redux/store.js
+++ practiceLogIn/src/redux/store.js
@@ -1,5 +1,34 @@
 import { createStore } from "redux";
 import userLogInReducer from "./reducers";
 
-const store = createStore(userLogInReducer)
+
+function loadState(){
+ try{
+  const returnedState = localStorage.getItem("authState");
+  if(returnedState === null) 
+    {return undefined}
+  return JSON.parse(returnedState)
+ } catch(error)
+{ console.error("Failed to load authState from localStorage", error);
+  return undefined};
+   
+}
+
+const existedState = loadState();
+
+const store = createStore(userLogInReducer,existedState)
+
+store.subscribe(() => {
+  try{
+    const state = store.getState();
+    const stateToSave = {
+      user: state.user,
+      isAuthenticated: state.isAuthenticated,
+    };
+    const returnedState = JSON.stringify(stateToSave);
+    localStorage.setItem("authState", returnedState);
+    } catch(error){console.error("Failed to load authState from localStorage", error);} } )
+
+
+
 export default store

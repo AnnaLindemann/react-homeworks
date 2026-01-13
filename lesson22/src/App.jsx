1eec3b6b2f26f646865c30b2649b92a60c25613f
Anna <anshalneva@gmail.com>
2026-01-13 20:17:10 +0100

    Added async logic

--- lesson22/src/App.jsx
+++ lesson22/src/App.jsx
@@ -1,14 +1,41 @@
-
 import './App.css'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { getUsers } from './redux/slices/userSlice'
+import { useEffect } from 'react'
 function App() {
+  const dispatch = useDispatch()
+  const {users,status, error} = useSelector((state) => state.user)
+ 
+  function handleClick() {
+    dispatch(getUsers())
+  }
+
+  useEffect(()=> {
+    dispatch(getUsers())
+  },[dispatch]);
+
+if(status === "loading"){
+  return <h2>Loading...</h2>
+} 
+
+if(status === "failed"){
+  return <h2>Error:{error}</h2>
+}
 
 
   return (
     <>
-      
+      <button onClick={handleClick}>fetchUsers</button>
+      {status === "succeeded" && (
+        users.map((user) => {
+          return <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>{user.website}</p>
+            <hr/>
+          </div>
+        })
+      )}
     </>
   )
 }
-
 export default App
--- lesson22/src/redux/slices/userSlice.js
+++ lesson22/src/redux/slices/userSlice.js
@@ -1,40 +1,32 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
-const BASE_URL =  "https://jsonplaceholder.typicode.com";
-
-export const getUsers = createAsyncThunk("user/getUsers", async () => {
-  const response = await axios.get(`${BASE_URL}/users`);
-  return response.data;
+export const getUsers = createAsyncThunk('user/getUsers', async () => {
+  const response = await axios(`${BASE_URL}/users`)
+  return response.data
 })
-
-dispatchEvent(getUsers("34276574326573"))
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
-    data: [],
+    users: [],
+    status: 'idle', // idle, loading, succeeded, failed
+    error: null,
   },
-  reducers:{
-    addTodo(state,action) {}
-  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(getUsers.pending, (state,action) => {
+      state.status = "loading"
+    })
+    .addCase(getUsers.fulfilled, (state,action) => {
+      state.status = "succeeded";
+      state.users = action.payload
+    })
+    .addCase(getUsers.rejected, (state,action) => {
+      state.status = "failed"
+      state.error = action.error.message
+    })
+  }
 })
-
-// export default userSlice.reducer
-
-// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
-// const BASE_URL = "https://jsonplaceholder.typicode.com";
-// export const getUsers = createAsyncThunk("user/getUsers", async () => {
-//   const response = await axios.get(`${BASE_URL}/users`);
-//   return response.data;
-// });
-// const userSlice = createSlice({
-//   name: "user",
-//   initialState: {
-//     data: [],
-//   },
-//   reducers: {
-//     addTodo(state, action) {},
-//   },
-// });
-// export default userSlice.reducer;
+export default userSlice.reducer

938a65e0be57d57144f9e8d24291ce690c2e0126
Anna <anshalneva@gmail.com>
2026-01-21 14:28:30 +0100

    Added components BreadCrambs and Categories

---
+++ finalProject/Pet-Shop-Frontend/src/components/breadCrambs/index.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+
+function BreadCrumbs({ items }) {
+  return (
+    <nav className={styles.breadCrumbs}>
+      {items.map((item, index) => {
+        const isLast = index === items.length - 1;
+
+        return (
+          <div className={styles.breadCrumbsItem} key={index}>
+            {item.to && !isLast ? (
+              <Link to={item.to} className={styles.breadCrumbsLink}>
+                {item.label}
+              </Link>
+            ) : (
+              <span className={styles.breadCrumbsCurrent}>
+                {item.label}
+              </span>
+            )}
+
+            {!isLast && <span className={styles.breadCrumbsLine} />}
+          </div>
+        );
+      })}
+    </nav>
+  );
+}
+
+export default BreadCrumbs;
--- finalProject/Pet-Shop-Frontend/src/pages/categories/index.jsx
+++ finalProject/Pet-Shop-Frontend/src/pages/categories/index.jsx
@@ -1,4 +1,70 @@
-export default function Categories(){
+import BreadCrumbs from "../../components/breadCrambs"
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css"
+import { useDispatch,useSelector } from "react-redux";
+import { useEffect } from "react";
+import { categoriesGet } from "../../redux/slices/categoriesSlice";
+import { API_URL } from "../../api/api";
 
-  return <h2>Categories</h2>
+
+export const BREADCRUMBS = {
+    categories: [
+    { label: "Main page", to: "/" },
+    { label: "Categories" }
+  ],  
+};
+
+export default function Category(){
+const dispatch = useDispatch()
+const {items, error, status} = useSelector((state) => state.categories)
+
+useEffect(() => {
+if (status === "idle") 
+  {dispatch(categoriesGet())}
+ },[status,dispatch])
+
+ let content = null;
+ 
+ if(status === "loading"){
+  content = (
+    <>
+        <div className={styles.skeletonCard} />
+        <div className={styles.skeletonCard} />
+        <div className={styles.skeletonCard} />
+        <div className={styles.skeletonCard} />
+      </>
+  )
+ } else if(status === "failed") {
+  content =(
+  <div className={styles.errorBox}>
+        <p className={styles.errorText}>{error.message}</p>
+        <button
+          className={styles.retryBtn}
+          type="button"
+          onClick={() => dispatch(categoriesGet())}
+        >
+          Retry
+        </button>
+      </div>)
+ } else if (status === "succeeded") {
+  content = items.map((item) => (
+    <Link key={item.id} className={styles.card} to={`/categories/${item.id}`}>
+      <img className={styles.cardImg} src={`${API_URL}${item.image}`} alt={item.title} />
+        <span className={styles.cardTitle}>{item.title}</span>
+    </Link>
+  ))
+ }
+
+  return (
+  <section className={styles.section}>
+    <div>
+      <BreadCrumbs items={BREADCRUMBS.categories}/>
+    </div>
+  <div className={styles.sectionHead}>
+    <h2 className={styles.title}>Categories</h2>
+  </div>
+  <div className={styles.grid}>{content}</div>
+  </section>
+    
+  )
 }
